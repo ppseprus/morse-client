e96a47d448e45d4e5403ad3f129b30b977100e52
Peter Seprus <peter_seprus@epam.com>
2016-01-18 16:51:32 +0100

    angular routes declared

---
+++ app/app.ts
@@ -0,0 +1,32 @@
+(function() {
+    'use strict';
+    
+    angular
+        .module('morse', [
+            'ui.router'
+        ]);
+    
+    angular
+        .module('morse')
+        .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+            
+            $urlRouterProvider.otherwise('/main');
+            
+            $stateProvider 
+                .state('main', {
+                    url: '/main',
+                    views: {
+                        'main': {
+                            templateUrl: 'app/main/main.html',
+                            controller: 'mainController'
+                        }
+                    },
+                    params: {
+                        inputText: ''
+                    }
+                    
+                });
+                
+        }]);
+    
+})();
