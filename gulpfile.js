c2f3237249d977b4a8e832ba5022ecefd67ba0c6
Peter Seprus <peter_seprus@epam.com>
2016-01-18 17:02:52 +0100

    gulps task created for scss, package.json updated accordingly

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,29 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var ts = require('gulp-typescript');
-//var sass = require('gulp-sass');
+var sass = require('gulp-sass');
 
 gulp.task('build-js', function () {
     return gulp
 		.src('./app/**/*.ts')
         .pipe(ts({
-            noImplicitAny: true,
-            out: 'main.min.js'
-        }))
+                noImplicitAny: true,
+                out: 'main.min.js'
+            }))
 		.pipe(uglify())
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest('./build'));
 });
 
-//gulp.task('watch-js', ['build-js'], function() {
-//	gulp.watch('./app/**/*.ts', ['ts-js']);
-//});
-
-// default task
-gulp.task('default', [], function() {
-		
-	// default task
+gulp.task('build-scss', function () {
+    return gulp
+		.src('./assets/sass/*.scss')
+        .pipe(sass({
+                outputStyle: 'compressed'
+            }))
+        .pipe(gulp.dest('./build'));
 });
+
+gulp.task('build', ['build-js', 'build-scss'], function() {});
+
+
+gulp.task('default', [], function() {});
