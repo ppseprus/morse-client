9ba9438f9d9740e9727c501323b1ce7ebf06563b
Peter Seprus <peter_seprus@epam.com>
2016-01-18 16:51:55 +0100

    main module created

---
+++ app/main/main.controller.ts
@@ -0,0 +1,12 @@
+(function(){
+	'use strict';
+	
+	angular
+		.module('morse')
+		.controller('mainController', ['$scope', '$state',
+			function ($scope, $state) {
+				
+				
+				
+			}]);
+})();
