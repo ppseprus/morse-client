f20143ec9d402eafd757f1ea8ec5709b6b5b9deb
Peter Seprus <peter_seprus@epam.com>
2016-01-20 19:07:38 +0100

    main controller updated

--- app/main/main.controller.ts
+++ app/main/main.controller.ts
@@ -4,25 +4,58 @@
 	'use strict';
 	angular
 		.module('morse')
-		.controller('mainController', ['$scope', '$state', 'socket', '$timeout',
-			function ($scope, $state, socket, $timeout) {
+		.controller('mainController', ['$state', '$scope', 'socket', '$filter',
+			function ($state, $scope, socket, $filter) {
 				
-				socket.on('messages', function (data) {
-					$timeout(function(){
-						$scope.messages.push({
-							dt: data.timestamp,
-							msg: data.message
+				//	initialise message container
+				$scope.messages = [];
+				
+				//	initialise logging
+				function logMessage(data){
+					$scope.messages.push({
+						ev: data.event.replace(':',''),
+						dt: data.timestamp,
+						usr: data.username,
+						msg: data.message
+					});
+				}
+				
+				//	select events to listen to
+				var eventsToListen = {
+					'user:connect': true,
+					'user:disconnect': true,
+					'user:join': true,
+					'message': true
+				};
+				
+				//	initialise event listeners
+				angular.forEach(eventsToListen, function(value, e){
+					if(value) {
+						socket.on(e, function(data){
+							logMessage(data);
 						});
-					})
+					}
 				});
 				
-				
-				socket.emit('join', 'ppseprus');
-				
-				socket.emit('messages', {
-					'timestamp': new Date(),
-					'message': 'testing 123...'
+				//	TEMPORARY
+				socket.emit('user:join', {
+					'username':'ppseprus'
 				});
+				
+				//	message sending
+				$scope.sendMessage = function(isValid) {
+					if (isValid) {
+						socket.emit('message', {
+							'timestamp': $filter('date')(new Date, 'HH:mm:ss.sss', 'UTC'),
+							'message': $scope.message
+						});
+						
+						$scope.message = '';
+						
+						$scope.console.$setPristine();
+						$scope.console.$setUntouched();
+					}
+				};
 				
 			}]);
 })();
